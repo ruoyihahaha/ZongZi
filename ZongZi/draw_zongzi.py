7498dc1b0eb9ed9bfb3daf38a362b26e7599caaa
ruoyihahaha <2927176763@qq.com>
2025-05-28 11:28:46 +0800

    Update draw_zongzi.py

--- ZongZi/draw_zongzi.py
+++ ZongZi/draw_zongzi.py
@@ -2,19 +2,14 @@ import turtle as t
 import pygame
 from hello_zongzi import ZongZi,play_music
 
-t.speed(5)
-t.screensize(512,512,"white")
-t.pencolor("black")
-t.pensize(5)
-t.penup()
-t.goto(100,-100)
-t.pendown()
-
+#初始化音乐
 pygame.mixer.init()
 music_file = "bgm.mp3"
 # 先播放音乐，再开始画图
 play_music(music_file)
+
 zongzi = ZongZi()
 zongzi.draw()
+
 t.done()
 pygame.mixer.music.stop()
